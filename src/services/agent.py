29d56cc24ec775826799cc13ed9efeccc9cc6b2a
GlobalYam <aarni.hellen@gmail.com>
2024-09-03 13:05:26 +0300

    agents

---
+++ src/services/agent.py
@@ -0,0 +1,3 @@
+class Agent:
+    def __init__(self, role) -> None:
+        self.role = str(role)
---
+++ src/services/agent_manager.py
@@ -0,0 +1,9 @@
+from agent.py import Agent
+
+
+class AgentManager:
+    def __init__(self) -> None:
+        self.list_of_agents = []
+
+    def add_agent(self, role_text):
+        self.list_of_agents.append(Agent(role_text))
