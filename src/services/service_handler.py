573401c10491f5da7e321ce08e9a87a2c17da363
GlobalYam <aarni.hellen@gmail.com>
2024-09-04 13:46:18 +0300

    servicehandler fixes

--- src/services/service_handler.py
+++ src/services/service_handler.py
@@ -12,29 +12,32 @@ class ServiceHandler:
             if len(self.io.inputs) == 0:
                 self.io.add_input("command: ")
             command = self.io.read()
-            if command == "exit":
-                break
-            elif command == "add":
-                self.io.add_input("role: ")
-                role = self.io.read()
-                self.add_agent(role)
-            elif command == "prompt":
-                if len(self.io.inputs) == 0:
-                    self.io.add_input("prompt: ")
-                prompt = self.io.read()
-                output = self.text_in_text_out(prompt)
-                self.io.write(output)
-            else:
-                self.io.write("Invalid command")
+            match command:
+                case "exit":
+                    break
+                case "add":
+                    self.io.add_input("role: ")
+                    role = self.io.read()
+                    self.add_agent(role)
+                case "prompt":
+                    if len(self.io.inputs) == 0:
+                        self.io.add_input("prompt: ")
+                    prompt = self.io.read()
+                    output = self.text_in_text_out(prompt)
+                    self.io.write(output)
+                case _:
+                    self.io.write("Invalid command")
 
     def info(self):
-        self.io.write(self.text_in_text_out('Commands:'))
-        self.io.write('add - Add a new agent')
-        self.io.write('prompt - Generate outputs from agents')
-        self.io.write('exit - Exit the program')
+        self.io.write("Commands:")
+        self.io.write("add - Add a new agent")
+        self.io.write("prompt - Generate outputs from agents")
+        self.io.write("exit - Exit the program")
 
     def text_in_text_out(self, text):
-        return f"{text} {[str(agent.role) for agent in self.agent_manager.list_of_agents]}"
+        return (
+            f"{text} {[str(agent.role) for agent in self.agent_manager.list_of_agents]}"
+        )
 
     def create_agents(self, list_of_roles):
         for role in list_of_roles:
