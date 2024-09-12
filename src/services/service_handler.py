f844ada690cbfb3c6af683ed39254839ec14f355
tumffa <tuomas.v.niemi@gmail.com>
2024-09-12 10:51:18 +0300

    Remove command line interface

--- src/services/service_handler.py
+++ src/services/service_handler.py
@@ -6,35 +6,6 @@ class ServiceHandler:
         default_agents = ["farmer", "eld", "student"]
         self.create_agents(default_agents)
 
-    def start(self):
-        self.info()
-        while True:
-            self.io.write("")
-            if len(self.io.inputs) == 0:
-                self.io.add_input("command: ")
-            command = self.io.read()
-            match command:
-                case "exit":
-                    break
-                case "add":
-                    self.io.add_input("role: ")
-                    role = self.io.read()
-                    self.add_agent(role)
-                case "prompt":
-                    if len(self.io.inputs) == 0:
-                        self.io.add_input("prompt: ")
-                    prompt = self.io.read()
-                    output = self.text_in_text_out(prompt)
-                    self.io.write(output)
-                case _:
-                    self.io.write("Invalid command")
-
-    def info(self):
-        self.io.write("Commands:")
-        self.io.write("add - Add a new agent")
-        self.io.write("prompt - Generate outputs from agents")
-        self.io.write("exit - Exit the program")
-
     def text_in_text_out(self, text):
         prompt_list = self.format_prompt_list(text)
         return "prompts:\n" + "\n".join([str(prompt) for prompt in prompt_list])
