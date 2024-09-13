a414ab80d5027ec24dbda02941a9f2cb8d0fb351
Niko Raulas <niko.raulas@outlook.com>
2024-09-13 14:23:42 +0300

    add tasks for pylint and tests

--- tasks.py
+++ tasks.py
@@ -5,3 +5,11 @@ def coverage(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
     ctx.run("coverage report -m", pty=True)
     ctx.run("coverage html", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src", pty=True)
+
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
