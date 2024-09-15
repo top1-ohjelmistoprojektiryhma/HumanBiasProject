1f95c1dac5fed1826bd2d2544632e9ddf45ae37d
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2024-09-15 12:00:56 +0300

    add task for invoke backend and frontend

--- tasks.py
+++ tasks.py
@@ -1,15 +1,29 @@
 from invoke import task
 
+
 @task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
     ctx.run("coverage report -m", pty=True)
     ctx.run("coverage html", pty=True)
 
+
 @task
 def test(ctx):
     ctx.run("pytest src", pty=True)
 
+
 @task
 def lint(ctx):
     ctx.run("pylint src", pty=True)
+
+
+@task
+def backend(ctx):
+    ctx.run("source venv/bin/activate && python3 src/main.py", pty=True)
+
+
+@task
+def frontend(ctx):
+    with ctx.cd("src/ui"):
+        ctx.run("npm start", pty=True)
