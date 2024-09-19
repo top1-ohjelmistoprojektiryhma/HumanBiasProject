2996604e8dbcdbd6adf446e2353ad24841823ac9
tumffa <tuomas.v.niemi@gmail.com>
2024-09-19 13:08:12 +0300

    Add docstring to dialog_manager and dialog

---
+++ src/backend/services/dialog.py
@@ -0,0 +1,45 @@
+class Dialog:
+    """ Represents a dialog
+
+    Attributes:
+        initial_prompt (str): The initial prompt for the dialog
+        rounds (dict): A dictionary of rounds identified by round number
+    """
+    def __init__(self, initial_prompt=""):
+        self.initial_prompt = initial_prompt
+        self.rounds = {}
+
+    def add_round(self, round_num, prompts):
+        """ Add a round to the dialog
+
+        Args:
+            round_num (int): The round number
+            prompts (list): A list of prompts for the round
+        """
+        self.rounds[round_num] = prompts
+
+    def __str__(self):
+        def truncate(text, length=30):
+            return text[:length] + '...' if len(text) > length else text
+
+        formatted_rounds = ",\n  ".join(
+            f"{k}: ["
+            + ", \\\n    ".join(
+                str({
+                    'agent': p['agent'],
+                    'model': p['model'],
+                    'input': truncate(p['input']),
+                    'output': truncate(p['output'])
+                })
+                for p in v
+            )
+            + "]"
+            for k, v in self.rounds.items()
+        )
+
+        return (
+            f"{{\n"
+            f"  'initial_prompt': '{self.initial_prompt}',\n"
+            f"  'rounds': {{\n  {formatted_rounds}\n  }}\n"
+            f"}}"
+        )
---
+++ src/backend/services/dialog_manager.py
@@ -0,0 +1,44 @@
+from .dialog import Dialog
+
+class DialogManager:
+    """ Manages dialog objects
+
+    Attributes:
+        dialogs: A dictionary of dialog objects
+    """
+    def __init__(self):
+        self.dialogs = {}
+
+    def new_dialog(self, initial_prompt):
+        """ Create a new dialog object
+
+        Args:
+            initial_prompt (str): The initial prompt for the dialog
+
+        Returns:
+            int: The dialog id
+            Dialog: The dialog object
+        """
+        new_id = len(self.dialogs)
+        dialog = Dialog(initial_prompt)
+        self.dialogs[new_id] = dialog
+        return new_id, dialog
+
+    def add_round_to_dialog(self, dialog_id, round_num, prompts):
+        """ Add a round to a dialog object
+
+        Args:
+            dialog_id (int): The id of the dialog object
+            round_num (int): The round number
+            prompts (list): A list of prompts for the round
+        """
+        self.dialogs[dialog_id].add_round(round_num, prompts)
+
+    def get_dialog(self, dialog_id):
+        return self.dialogs[dialog_id]
+
+    def delete_dialog(self, dialog_id):
+        del self.dialogs[dialog_id]
+
+    def all_dialogs(self):
+        return self.dialogs
