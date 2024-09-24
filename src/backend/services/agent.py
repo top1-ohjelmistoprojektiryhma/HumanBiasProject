37aaeaf6d317b003047583139271c695a73ed29a
tumffa <tuomas.v.niemi@gmail.com>
2024-09-24 11:16:59 +0300

    Add dialog history to agent object

--- src/backend/services/agent.py
+++ src/backend/services/agent.py
@@ -2,8 +2,28 @@ class Agent:
     """Class representing a singular agent, currently stroring only the role but left to be expanded
     Attributes:
         role (str): The role of the agent
-
+        histories (dict): A dictionary of dialog histories identified by dialog id
     """
-
     def __init__(self, role) -> None:
         self.role = str(role)
+        self.histories = {}
+
+    def add_chat_to_history(self, dialog_id, chat):
+        """Add a chat to the agent's history
+        Args:
+            dialog_id (str): The dialog id
+            chat (list): list of chat messages in the format {"role": "user"/"model", "text": "message"}
+        """
+        if dialog_id not in self.histories:
+            self.histories[dialog_id] = chat
+            return
+        self.histories[dialog_id].extend(chat)
+
+    def get_history(self, dialog_id):
+        """Get the dialog history for a specific dialog
+        Args:
+            dialog_id (str): The dialog id
+        Returns:
+            list: A list of dictionaries representing the dialog history
+        """
+        return self.histories[dialog_id]
--- src/backend/services/dialog.py
+++ src/backend/services/dialog.py
@@ -18,7 +18,7 @@ class Dialog:
         """
         self.rounds[round_num] = prompts
 
-    def get_history(self, agent_obj):
+    def get_agent_history(self, agent_obj):
         """ Get the dialog history for a specific agent
 
         Args:
@@ -33,7 +33,7 @@ class Dialog:
                 if p['agent'] == agent_obj:
                     history.append({"role": "user", "text": p['input']})
                     history.append({"role": "model", "text": p['output']})
-        return history
+        return history 
 
     def to_dict(self):
         init = self.initial_prompt
