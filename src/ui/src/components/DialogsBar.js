e0087be1b83f1ba71ffa486ff773c8ca0a3f84f2
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2024-09-28 17:44:58 +0300

    make rounds expandable in dialogs bar

--- src/ui/src/components/DialogsBar.js
+++ src/ui/src/components/DialogsBar.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const DialogsBar = ({ dialogs, expandedDialogs, toggleDialog }) => {
-  // Funktio, joka käsittelee dialogin laajentamisen ja pienentämisen
+  const [expandedRounds, setExpandedRounds] = useState({});
+
   const handleToggleDialog = (dialogId) => {
     toggleDialog((prevState) => ({
       ...prevState,
-      [dialogId]: !prevState[dialogId], // Käännä tila
+      [dialogId]: !prevState[dialogId],
+    }));
+  };
+
+  const handleToggleRound = (dialogId, roundId) => {
+    setExpandedRounds((prevState) => ({
+      ...prevState,
+      [dialogId]: {
+        ...prevState[dialogId],
+        [roundId]: !prevState[dialogId]?.[roundId],
+      },
     }));
   };
 
@@ -18,27 +29,29 @@ const DialogsBar = ({ dialogs, expandedDialogs, toggleDialog }) => {
         ) : (
           Object.keys(dialogs).map((dialogId) => (
             <li key={dialogId}>
-              {/* Klikattava initial prompt */}
               <strong onClick={() => handleToggleDialog(dialogId)} style={{ cursor: 'pointer' }}>
                 Dialog {dialogId}: {dialogs[dialogId].initial_prompt}
               </strong>
-              
-              {/* Näytä yksityiskohdat vain, jos dialogi on avattu */}
               {expandedDialogs[dialogId] && (
                 <ul>
                   {Object.keys(dialogs[dialogId].rounds).map((roundId) => (
                     <li key={roundId}>
-                      <strong>Round {roundId}:</strong>
-                      <ul>
-                        {dialogs[dialogId].rounds[roundId].map((prompt, index) => (
-                          <li key={index}>
-                            <strong>Agent:</strong> {prompt.agent} <br />
-                            <strong>Model:</strong> {prompt.model} <br />
-                            <strong>Input:</strong> {prompt.input} <br />
-                            <strong>Output:</strong> {prompt.output}
-                          </li>
-                        ))}
-                      </ul>
+                      <strong onClick={() => handleToggleRound(dialogId, roundId)} style={{ cursor: 'pointer' }}>
+                        Round {roundId}
+                      </strong>
+
+                      {expandedRounds[dialogId]?.[roundId] && (
+                        <ul>
+                          {dialogs[dialogId].rounds[roundId].map((prompt, index) => (
+                            <li key={index}>
+                              <strong>Agent:</strong> {prompt.agent} <br />
+                              <strong>Model:</strong> {prompt.model} <br />
+                              <strong>Input:</strong> {prompt.input} <br />
+                              <strong>Output:</strong> {prompt.output}
+                            </li>
+                          ))}
+                        </ul>
+                      )}
                     </li>
                   ))}
                 </ul>
