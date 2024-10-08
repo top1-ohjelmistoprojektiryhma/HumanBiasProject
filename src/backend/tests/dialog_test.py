c488ceff884b7863043a9e6c0d9d36949651c480
Taina Hilliaho <taina.hilliaho@helsinki.fi>
2024-10-08 20:38:51 +0300

    Fix dialog tests

--- src/backend/tests/dialog_test.py
+++ src/backend/tests/dialog_test.py
@@ -6,12 +6,15 @@ class ExampleAgent:
     def __init__(self):
         self.role = "student"
 
+
 class TestDialog(unittest.TestCase):
     def setUp(self):
         self.agents = [ExampleAgent(), ExampleAgent()]
         self.dialog = Dialog(
-            "Initial prompt", {self.agents[0]: "None", self.agents[1]: "None"}, "dialog"
-            )
+            "Initial prompt",
+            {self.agents[0]: "None", self.agents[1]: "None"},
+            "dialog - no consensus",
+        )
 
     def test_add_round_works(self):
         self.dialog.add_round(1, ["Prompt 1", "Prompt 2"])
@@ -24,61 +27,57 @@ class TestDialog(unittest.TestCase):
         self.assertEqual(self.dialog.get_next_agent(), self.agents[1])
 
     def test_to_dict_works(self):
-        self.dialog.add_round(1, [
-            {
-                'agent': ExampleAgent(),
-                'model': 'model',
-                'input': 'input',
-                'output': 'output'
-            }
-        ])
+        self.dialog.add_round(
+            1,
+            [
+                {
+                    "agent": ExampleAgent(),
+                    "model": "model",
+                    "input": "input",
+                    "output": "output",
+                }
+            ],
+        )
         self.assertEqual(
             self.dialog.to_dict(),
             {
-                'initial_prompt': 'Initial prompt',
-                'rounds': {
+                "initial_prompt": "Initial prompt",
+                "rounds": {
                     1: [
                         {
-                            'agent': 'student',
-                            'model': 'model',
-                            'input': 'input',
-                            'output': 'output'
+                            "agent": "student",
+                            "model": "model",
+                            "input": "input",
+                            "output": "output",
                         }
                     ]
-                }
-            }
+                },
+            },
         )
 
     def test_string(self):
-        self.dialog.add_round(1, [
-            {
-                'agent': {'role': 'agent'},
-                'model': 'model',
-                'input': 'input',
-                'output': 'output'
-            }
-        ])
+        self.dialog.add_round(
+            1,
+            [
+                {
+                    "agent": {"role": "agent"},
+                    "model": "model",
+                    "input": "input",
+                    "output": "output",
+                }
+            ],
+        )
         # dont include in coverage
         print(self.dialog)
         self.assertEqual(True, True)
 
     def test_get_agent_history_works(self):
         agent = ExampleAgent()
-        self.dialog.add_round(1, [
-            {
-                'agent': agent,
-                'model': 'model',
-                'input': 'input',
-                'output': 'output'
-            }
-        ])
-        self.assertEqual(self.dialog.get_agent_history(agent), [
-            {
-                'role': 'user',
-                'text': 'input'
-            },
-            {
-                'role': 'model',
-                'text': 'output'
-            }
-        ])
+        self.dialog.add_round(
+            1,
+            [{"agent": agent, "model": "model", "input": "input", "output": "output"}],
+        )
+        self.assertEqual(
+            self.dialog.get_agent_history(agent),
+            [{"role": "user", "text": "input"}, {"role": "model", "text": "output"}],
+        )
