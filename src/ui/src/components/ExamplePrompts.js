0c8b8b6caebd3e14412cd729b2b8e94f072ca3f1
gitelras <ella.rasalo@helsinki.fi>
2024-10-11 16:17:41 +0300

    design example promt box

--- src/ui/src/components/ExamplePrompts.js
+++ src/ui/src/components/ExamplePrompts.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 const ExamplePrompts = () => {
     return (
-        <div>
-            <h3>Example prompts for testing:</h3>
-            <ul>
-                <li>Finland's education system should be updated for the digital age.</li>
-                <li>The European Union should invest massively in renewable energy.</li>
-                <li>Political parties should create a unified strategy to strengthen cybersecurity.</li>
-            </ul>
+        <div className="example-prompts">
+            <div>
+                Finland's education system should be updated for the digital age.
+            </div>
+            <div>
+                The European Union should invest massively in renewable energy.
+            </div>
+            <div>
+                Political parties should create a unified strategy to strengthen cybersecurity.
+            </div>
         </div>
     );
 };
