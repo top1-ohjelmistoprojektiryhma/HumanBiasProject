0572bfd453301ba526268cc552301fb29774f130
gitelras <ella.rasalo@helsinki.fi>
2024-10-13 01:30:03 +0300

    remove dots

--- src/ui/src/components/ExamplePrompts.js
+++ src/ui/src/components/ExamplePrompts.js
@@ -3,13 +3,13 @@ import React from 'react';
 const ExamplePrompts = ({ setPrompt }) => {
     return (
         <div className="example-prompts">
-            <div onClick={() => setPrompt("Finland's education system should be updated for the digital age.")}>
+            <div onClick={() => setPrompt("Finland's education system should be updated for the digital age")}>
                 Finland's education system should be updated for the digital age
             </div>
-            <div onClick={() => setPrompt("The European Union should invest massively in renewable energy.")}>
+            <div onClick={() => setPrompt("The European Union should invest massively in renewable energy")}>
                 The European Union should invest massively in renewable energy
             </div>
-            <div onClick={() => setPrompt("Political parties should create a unified strategy to strengthen cybersecurity.")}>
+            <div onClick={() => setPrompt("Political parties should create a unified strategy to strengthen cybersecurity")}>
                 Political parties should create a unified strategy to strengthen cybersecurity
             </div>
         </div>
