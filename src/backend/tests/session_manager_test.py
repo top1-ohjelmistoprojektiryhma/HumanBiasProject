fb9c8998fe10d94a66ef34fc19172eb305f179f6
Niko Raulas <niko.raulas@outlook.com>
2024-10-27 23:37:04 +0200

    add more tests

--- src/backend/tests/session_manager_test.py
+++ src/backend/tests/session_manager_test.py
@@ -8,8 +8,10 @@ class TestSessionManager(unittest.TestCase):
         self._manager = SessionManager()
         self.consensus_mock = Mock()
         self.no_consensus_mock = Mock()
-        self._manager._all_formats = {"dialog - no consensus":self.no_consensus_mock,
-                                      "dialog - consensus":self.consensus_mock}
+        self.user_comments_mock = Mock()
+        self._manager._all_formats = {"dialog - no consensus": self.no_consensus_mock,
+                                      "dialog - consensus": self.consensus_mock,
+                                      "dialog - user comments": self.user_comments_mock}
 
     def test_new_session_no_consensus_works(self):
         session_id, session = self._manager.new_session(
@@ -41,3 +43,28 @@ class TestSessionManager(unittest.TestCase):
             "Initial prompt", agents=None, session_format="dialog - no consensus"
         )
         self.assertEqual(self._manager.all_sessions(), {session_id: session.to_dict()})
+
+    def test_get_all_formats_works(self):
+        result = self._manager.get_all_formats()
+        self.assertListEqual(result, ["dialog - no consensus",
+                                  "dialog - consensus",
+                                  "dialog - user comments"])
+
+    def test_get_session_prompst_works(self):
+        session_mock = Mock()
+        session_mock.get_prompts.return_value = ["test prompt"]
+        self._manager._sessions["id_1"] = session_mock
+        result = self._manager.get_session_prompts("id_1")
+        self.assertListEqual(result, ["test prompt"])
+
+    def test_update_session_with_comment_works(self):
+        session_mock = Mock()
+        self._manager._sessions["id_1"] = session_mock
+        self._manager.update_session_with_comment("id_1", "test comment")
+        session_mock.update_with_comment.assert_called_with("test comment")
+
+    def test_update_session_with_responses_works(self):
+        session_mock = Mock()
+        self._manager._sessions["id_1"] = session_mock
+        self._manager.update_session_with_responses("id_1", "response")
+        session_mock.update_with_responses.assert_called_with("response")
